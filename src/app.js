cff4ef8baffcb3675d59f1a4124eeeec60ab7045
dvera17 <dsvera174@gmail.com>
2023-08-18 17:14:27 -0500

    feat: :sparkles: Implementacion de algunos endpoints
    Implementacion de algunos endpoints

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import passportHelper from './helpers/passportHelper.js';
 import { crearToken } from './middlewares/mwJWT.js';
 import validateRol from './middlewares/mwValidateRol.js';
 import routerUsuario from './routes/usuario.routes.js';
+import routerCita from './routes/cita.routes.js';
+import routerMedico from './routes/medico.routes.js';
 import { config } from "dotenv";
 config();
 const app = express();
@@ -10,8 +12,11 @@ app.set('port', process.env.PORT_SERVER);
 app.use(express.json());
 app.use(passportHelper.initialize());
 
+const bearerAuth = passportHelper.authenticate('bearer', { session: false })    
+
 app.use('/token/:usuario', crearToken)
-app.use('/api/admin', routerUsuario)
-app.use('/api/vendedor', validateRol, (req, res) => { res.send(`In vendedor`) })
+app.use('/api/admin/pacientes', bearerAuth, validateRol, routerUsuario)
+app.use('/api/admin/cita', bearerAuth, validateRol, routerCita)
+app.use('/api/admin/medico', bearerAuth, validateRol, routerMedico)
 
 export default app;
